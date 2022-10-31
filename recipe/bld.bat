1245ea9afab5bbc9cf913f1d4d073dceac1b6eee
Peter Beaucage <peter.beaucage@nist.gov>
2022-10-31 03:35:43 -0400

    Fix typo

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,9 +4,9 @@ setlocal EnableDelayedExpansion
 mkdir build
 cd build
 cmake -G "NMake Makefiles" ^
-	-DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^	
+	-DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
 	-DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-  	-DCMAKE_BUILD_TYPE:STRING = Release ^ 
+  	-DCMAKE_BUILD_TYPE:STRING = Release ^
         .. ^
   	%CMAKE_ARGS% ^
 if errorlevel 1 exit 1
@@ -21,17 +21,16 @@ cd ../
 mkdir build-pybind
 cd build-pybind
 cmake -G "NMake Makefiles" ^
-	-DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^	
+	-DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
 	-DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-	.. ^
   	%CMAKE_ARGS% ^
   	-DCMAKE_BUILD_TYPE:STRING = Release ^
   	-DPYBIND=Yes ^
  	-DUSE_SUBMODULE_PYBIND=No ^
  	-DPython_EXECUTABLE="%PYTHON%"  ^
-  	-DCMAKE_INSTALL_PREFIX=${PREFIX} ^
   	%CMAKE_PLATFORM_FLAGS[@]% ^
-  	-DCMAKE_CUDA_RUNTIME_LIBRARY=Shared
+  	-DCMAKE_CUDA_RUNTIME_LIBRARY=Shared ^
+	.. ^
 if errorlevel 1 exit 1
 
 nmake
