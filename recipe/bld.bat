6a6ddeec33308dcfc411cfad80614c1eca1f5493
RobotoStation <tbm@nist.gov>
2022-10-30 20:54:05 -0400

    Fix env var syntax

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,8 +6,8 @@ cd build
 cmake -G "NMake Makefiles" ^
 	-DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^	
 	-DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-        ${SRC_DIR} ^
-  	${CMAKE_ARGS} ^
+        %SRC_DIR% ^
+  	%CMAKE_ARGS% ^
   	-DCMAKE_BUILD_TYPE:STRING = Release 
 if errorlevel 1 exit 1
 
@@ -23,14 +23,14 @@ cd build-pybind
 cmake -G "NMake Makefiles" ^
 	-DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^	
 	-DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-	${SRC_DIR} ^
-  	${CMAKE_ARGS} ^
+	%SRC_DIR% ^
+  	%CMAKE_ARGS% ^
   	-DCMAKE_BUILD_TYPE:STRING = Release ^
   	-DPYBIND=Yes ^
  	-DUSE_SUBMODULE_PYBIND=No ^
  	-DPython_EXECUTABLE="%PYTHON%"  ^
   	-DCMAKE_INSTALL_PREFIX=${PREFIX} ^
-  	${CMAKE_PLATFORM_FLAGS[@]} ^
+  	%CMAKE_PLATFORM_FLAGS[@]% ^
   	-DCMAKE_CUDA_RUNTIME_LIBRARY=Shared
 if errorlevel 1 exit 1
 
