c8f6b56bd2bec68b9ef269a12c2c20f84ada1c1c
Peter Beaucage <peter.beaucage@nist.gov>
2022-10-31 04:49:29 -0400

    Fix string formatting and line continuation

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,9 +6,10 @@ cd build
 cmake -G "NMake Makefiles" ^
 	-DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
 	-DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-  	-DCMAKE_BUILD_TYPE:STRING = Release ^
-        .. ^
+  	-DCMAKE_BUILD_TYPE:STRING="Release" ^
   	%CMAKE_ARGS% ^
+        ..
+
 if errorlevel 1 exit 1
 
 nmake
@@ -24,13 +25,13 @@ cmake -G "NMake Makefiles" ^
 	-DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
 	-DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   	%CMAKE_ARGS% ^
-  	-DCMAKE_BUILD_TYPE:STRING = Release ^
+  	-DCMAKE_BUILD_TYPE:STRING="Release" ^
   	-DPYBIND=Yes ^
  	-DUSE_SUBMODULE_PYBIND=No ^
  	-DPython_EXECUTABLE="%PYTHON%"  ^
   	%CMAKE_PLATFORM_FLAGS[@]% ^
   	-DCMAKE_CUDA_RUNTIME_LIBRARY=Shared ^
-	.. ^
+	.. 
 if errorlevel 1 exit 1
 
 nmake
