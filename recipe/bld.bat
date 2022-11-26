a310a7704e508cf6a498ec15b5acd3ef7c5b9da1
Peter Beaucage <github@peterbeaucage.com>
2022-11-26 17:41:00 -0500

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,21 +1,23 @@
 setlocal EnableDelayedExpansion
-
+echo on
 
 mkdir build
 cd build
+echo "In build, running Cmake"
 cmake -G "NMake Makefiles" ^
 	-DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
 	-DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   	-DCMAKE_BUILD_TYPE:STRING="Release" ^
 	-DCMAKE_CUDA_HOST_COMPILER:PATH="%CXX%" ^
+  	${CMAKE_PLATFORM_FLAGS[@]} ^
 	${CMAKE_ARGS} ^
         ..
 
 if errorlevel 1 exit 1
-
+echo "running nmake"
 nmake
 if errorlevel 1 exit 1
-
+echo "running nmake install"
 nmake install
 if errorlevel 1 exit 1
 cd ../
